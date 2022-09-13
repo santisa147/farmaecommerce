0a7c68fa0f477b080a65898c54661683acfc0a2d
Santiago Salinas <santisa147@gmail.com>
2022-09-13 20:50:20 -0300

    NavBar-MUI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import NavBar from './components/NavBar/NavBar';
+import '../src/main.css'
+
+export function App() {
+  
+  return (
+    
+    
+    <NavBar />
+    
+    
+  );
+}
+
+export default App;
